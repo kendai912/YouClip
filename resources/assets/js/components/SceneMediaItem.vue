ec4354e55c985e7bec02747f136b4df331dae7da
FashionCStar <fashioncstar@gmail.com>
2020-09-10 04:49:44 -0500

    ffmpeg test

--- resources/assets/js/components/SceneMediaItem.vue
+++ resources/assets/js/components/SceneMediaItem.vue
@@ -7,13 +7,14 @@
       class="pa-0 ma-0"
     >
       <v-col class="px-0" style="padding-top: 2px; paddint-bottom: 2px;">
-        <v-hover v-slot:default="{ hover }">
-          <v-card class="mx-auto" max-width="420" elevation="0" style="border-radius: 2px;">
+        <v-card class="mx-auto" max-width="420" elevation="0" style="border-radius: 2px;">
+          <v-hover v-slot:default="{ hover }">
             <v-img
+              v-if="!hover"
               v-on:click.stop="select(item)"
               class="white--text align-end"
               max-height="266.66px"
-              v-bind:src="hover ? '/storage/img/' + item.previewgif : item.preview"
+              v-bind:src="'/storage/img/' + item.preview"
               v-bind:alt="item.title"
               aspect-ratio="1.5"
             >
@@ -40,55 +41,57 @@
                 </div>
               </v-card-text>
             </v-img>
+            <video v-else controls autoplay muted style="width: 100%; height: 150px;">
+              <source v-bind:src="'/storage/videos/'+item.previewgif" type="video/mp4">
+            </video>
+          </v-hover>
+          <v-list-item class="px-2 pt-0 pb-2 mb-2">
+            <v-list width="55px" class="py-1 pl-1 pr-3">
+              <v-img src="/storage/icons/yt_social_red.png" />
+            </v-list>
+            <v-list-item-content>
+              <v-card-title class="pa-0" v-on:click.stop="select(item)">
+                <span class="home-and-search-result-title">{{ item.title }}</span>
+              </v-card-title>
+              <!-- <v-card-text class="text--primary">
+                <div v-on:click.stop="select(item)" class="grey--text text--darken-3">
+                  <span>{{ item.visitCount ? item.visitCount : 0 }}回視聴</span>
+                  <span style="font-size:8px;">&nbsp;&nbsp;&#8226;&nbsp;&nbsp;</span>
+                  <span>合計{{ item.totalDuration }}</span>
+                  <span style="font-size:8px;">&nbsp;&nbsp;&#8226;&nbsp;&nbsp;</span>
+                  <span>{{ item.timeSince }}前</span>
+                  <span
+                    v-if="item.likeCount"
+                    style="font-size:8px;"
+                  >&nbsp;&nbsp;&#8226;&nbsp;&nbsp;</span>
+                  <span v-if="item.likeCount">
+                    <i class="fas fa-heart my-grey-heart"></i>
+                    {{ item.likeCount}}
+                  </span>
+                </div>
+              </v-card-text> -->
+            </v-list-item-content>
+          </v-list-item>
 
-            <v-list-item class="px-2 pt-0 pb-2 mb-2">
-              <v-list width="55px" class="py-1 pl-1 pr-3">
-                <v-img src="/storage/icons/yt_social_red.png" />
-              </v-list>
-              <v-list-item-content>
-                <v-card-title class="pa-0" v-on:click.stop="select(item)">
-                  <span class="home-and-search-result-title">{{ item.title }}</span>
-                </v-card-title>
-                <!-- <v-card-text class="text--primary">
-                  <div v-on:click.stop="select(item)" class="grey--text text--darken-3">
-                    <span>{{ item.visitCount ? item.visitCount : 0 }}回視聴</span>
-                    <span style="font-size:8px;">&nbsp;&nbsp;&#8226;&nbsp;&nbsp;</span>
-                    <span>合計{{ item.totalDuration }}</span>
-                    <span style="font-size:8px;">&nbsp;&nbsp;&#8226;&nbsp;&nbsp;</span>
-                    <span>{{ item.timeSince }}前</span>
-                    <span
-                      v-if="item.likeCount"
-                      style="font-size:8px;"
-                    >&nbsp;&nbsp;&#8226;&nbsp;&nbsp;</span>
-                    <span v-if="item.likeCount">
-                      <i class="fas fa-heart my-grey-heart"></i>
-                      {{ item.likeCount}}
-                    </span>
-                  </div>
-                </v-card-text> -->
-              </v-list-item-content>
-            </v-list-item>
-
-            <!-- <v-card-text class="px-4">
-              <div class="horizontal-list-wrap block-chip-lines3">
-                <v-chip
-                  v-for="(tag, tagIndex) in item.tagArray"
-                  v-bind:key="item.id + '-' + tagIndex"
-                  class="my-tag-chip"
-                  small
-                  color="blue lighten-5"
-                  text-color="black"
-                  style="font-weight: normal"
-                >
-                  <v-avatar left>
-                    <i class="fas fa-tag my-black"></i>
-                  </v-avatar>
-                  {{ tag }}
-                </v-chip>
-              </div>
-            </v-card-text> -->
-          </v-card>
-        </v-hover>
+          <!-- <v-card-text class="px-4">
+            <div class="horizontal-list-wrap block-chip-lines3">
+              <v-chip
+                v-for="(tag, tagIndex) in item.tagArray"
+                v-bind:key="item.id + '-' + tagIndex"
+                class="my-tag-chip"
+                small
+                color="blue lighten-5"
+                text-color="black"
+                style="font-weight: normal"
+              >
+                <v-avatar left>
+                  <i class="fas fa-tag my-black"></i>
+                </v-avatar>
+                {{ tag }}
+              </v-chip>
+            </div>
+          </v-card-text> -->
+        </v-card>
       </v-col>
     </v-row>
     <LoadingItem v-if="isLoading" v-bind:numberOfItemsPerPagination="numberOfItemsPerPagination" />
