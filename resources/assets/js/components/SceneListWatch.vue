2f41479da11e625af7a05f4fad50aa8ea529c5d4
Vladislav Liyashenko <kad850147@gmail.com>
2020-11-25 20:47:09 +0100

    Fixed the styles and font sizes of the play list

--- resources/assets/js/components/SceneListWatch.vue
+++ resources/assets/js/components/SceneListWatch.vue
@@ -17,7 +17,8 @@
                   <v-avatar size="20">
                     <span>{{ parseInt(listIndex) + 1 }} {{panel}}</span>
                   </v-avatar>
-                  <v-img
+                  <v-hover v-slot:default="{ hover }">
+                    <v-img
                       class="white--text align-end"
                       v-bind:src="
                         hover
@@ -27,38 +28,40 @@
                       width="120"
                     >
                     </v-img>
+                  </v-hover>
+                  
                 </v-col>
-                <v-col cols="6" class="pa-1 py-3">
-                  <div class="pb-5">
-                    <v-img
-                      src="/storage/icons/yt_social_red.png"
-                      width="28px"
-                      max-height="28px"
-                      class="float-left mr-2"
-                    />
-                    <div>
-                      <span class="block-playlist-title" style="font-size: 14pt;">{{ mediaItems[listIndex].title }}</span>
-                    </div>
-                  </div>
+                <v-col cols="6" class="pa-1" style="display: flex; flex-direction: column; justify-content: space-between;">
                   <div>
                     <div>
-                      <span class="caption" style="font-size: 14pt !important; font-weight: bold;">{{
+                      <v-img
+                        src="/storage/icons/yt_social_red.png"
+                        width="28px"
+                        max-height="28px"
+                        class="float-left mr-2"
+                      />
+                      <div>
+                        <span class="block-playlist-title" style="font-size: 14pt;">{{ mediaItems[listIndex].title }}</span>
+                      </div>
+                    </div>
+                    <div>
+                      <span class="caption" style="font-size: 12pt !important; font-weight: bold;">{{
                           mediaItems[listIndex].start + "~" + mediaItems[listIndex].end
                         }}の場面</span>
                     </div>
-                    <div>
-                      <v-chip
-                        v-for="(tag, tagIndex) in readMore(mediaItems[listIndex].tagArray)"
-                        v-bind:key="mediaItems[listIndex].id + '-' + tagIndex"
-                        class="my-tag-chip px-2"
-                        color="lighten-5"
-                        text-color="my-grey"
-                        style="font-weight: normal; color: #808080; background-color: #ffffff !important; border: 1px solid #d7d7d7 !important;"
-                      >
-                        {{ tag }}
-                      </v-chip>
-                      <span v-if="mediaItems[listIndex].tagArray.length > 2">...</span>
-                    </div>
+                  </div>
+                  <div>
+                    <v-chip
+                      v-for="(tag, tagIndex) in readMore(mediaItems[listIndex].tagArray)"
+                      v-bind:key="mediaItems[listIndex].id + '-' + tagIndex"
+                      class="my-tag-chip px-2"
+                      color="lighten-5"
+                      text-color="my-grey"
+                      style="font-weight: normal; color: #808080; background-color: #ffffff !important; border: 1px solid #d7d7d7 !important;"
+                    >
+                      {{ tag }}
+                    </v-chip>
+                    <span v-if="mediaItems[listIndex].tagArray.length > 2">...</span>
                   </div>
                 </v-col>
             </v-row>
@@ -73,7 +76,7 @@
             class="pa-0 ma-0"
           >
             <v-col class="px-0">
-              <div>
+              <div v-bind:class="listIndex == index ? `yellow lighten-5` : ``">
                 <v-row class="ma-0">
                   <v-col cols="6" class="pa-1 d-flex align-center">
                     <v-avatar size="20">
@@ -94,86 +97,26 @@
                       </v-img>
                     </v-hover>
                   </v-col>
-                  <v-col cols="6" class="pa-1 py-3">
-                    <div class="pb-5">
-                      <v-img
-                        src="/storage/icons/yt_social_red.png"
-                        width="28px"
-                        max-height="28px"
-                        class="float-left mr-2"
-                      />
+                  <v-col cols="6" class="pa-1" style="display: flex; flex-direction: column; justify-content: space-between;">
+                    <div>
                       <div>
-                        <span class="block-playlist-title" style="font-size: 14pt;">{{ item.title }}</span>
+                        <v-img
+                          src="/storage/icons/yt_social_red.png"
+                          width="28px"
+                          max-height="28px"
+                          class="float-left mr-2"
+                        />
+                        <div>
+                          <span class="block-playlist-title" style="font-size: 14pt;">{{ item.title }}</span>
+                        </div>
                       </div>
-                    </div>
-                    <div class="pb-0">
                       <div>
-                        <span class="caption" style="font-size: 14pt !important; font-weight: bold;">{{
+                        <span class="caption" style="font-size: 12pt !important; font-weight: bold;">{{
                             item.start + "~" + item.end
                           }}の場面</span>
                       </div>
-                      <div>
-                        <v-chip
-                          v-for="(tag, tagIndex) in readMore(item.tagArray)"
-                          v-bind:key="item.id + '-' + tagIndex"
-                          class="my-tag-chip px-2"
-                          color="lighten-5"
-                          text-color="my-grey"
-                          style="font-weight: normal; color: #808080; background-color: #ffffff !important; border: 1px solid #d7d7d7 !important;"
-                        >
-                          {{ tag }}
-                        </v-chip>
-                        <span v-if="item.tagArray.length > 2">...</span>
-                      </div>
                     </div>
-                  </v-col>
-                </v-row>
-              </div>
-              <!-- <v-card
-                class="mx-auto"
-                max-width="420"
-                height="128"
-                elevation="1"
-              >
-                <v-row class="ma-0">
-                  <v-col cols="6" class="pa-1 d-flex align-center">
-                    <v-avatar size="20">
-                      <span>{{ index + 1 }}</span>
-                    </v-avatar>
-                    <v-hover v-slot:default="{ hover }">
-                      <v-img
-                        class="white--text align-end"
-                        v-bind:src="
-                          hover
-                            ? gifStoragePath + item.previewgif
-                            : thumbStoragePath + item.preview
-                        "
-                        v-bind:alt="item.title"
-                        aspect-ratio="1.7777"
-                        height="120"
-                      >
-                      </v-img>
-                    </v-hover>
-                  </v-col>
-                  <v-col cols="6" class="pa-1 py-3">
-                    <v-card-title class="pt-0">
-                      <v-img
-                        src="/storage/icons/yt_social_red.png"
-                        width="28px"
-                        max-height="28px"
-                        class="float-left mr-2"
-                      />
-                      <div>
-                        <span class="block-playlist-title" style="font-size: 14pt;">{{ item.title }}</span>
-                      </div>
-                    </v-card-title>
-                    <v-card-text class="pb-0">
-                      <div>
-                        <span class="caption" style="font-size: 14pt !important; font-weight: bold;">{{
-                            item.start + "~" + item.end
-                          }}の場面</span>
-                      </div>
-                      <div>
+                    <div>
                       <v-chip
                         v-for="(tag, tagIndex) in readMore(item.tagArray)"
                         v-bind:key="item.id + '-' + tagIndex"
@@ -186,77 +129,14 @@
                       </v-chip>
                       <span v-if="item.tagArray.length > 2">...</span>
                     </div>
-                    </v-card-text>
                   </v-col>
                 </v-row>
-              </v-card> -->
+              </div>
             </v-col>
           </v-row>
         </v-expansion-panel-content>
       </v-expansion-panel>
     </v-expansion-panels>
-    <!-- <v-card
-      class="mx-auto"
-      max-width="420"
-      height="128"
-      elevation="1"
-      v-if="panel !== 0"
-    >
-      <v-row class="ma-0">
-        <v-col cols="6" class="pa-1 d-flex align-center">
-          <v-avatar size="20">
-            <span>{{ parseInt(listIndex) + 1 }} {{panel}}</span>
-          </v-avatar>
-          <v-hover v-slot:default="{ hover }">
-            <v-img
-              class="white--text align-end"
-              v-bind:src="
-                hover
-                  ? gifStoragePath + mediaItems[listIndex].previewgif
-                  : thumbStoragePath + mediaItems[listIndex].preview
-              "
-              v-bind:alt="mediaItems[listIndex].title"
-              aspect-ratio="1.7777"
-              height="120"
-            >
-            </v-img>
-          </v-hover>
-        </v-col>
-        <v-col cols="6" class="pa-1 py-3">
-          <v-card-title class="pt-0">
-            <v-img
-              src="/storage/icons/yt_social_red.png"
-              width="28px"
-              max-height="28px"
-              class="float-left mr-2"
-            />
-            <div>
-              <span class="block-playlist-title" style="font-size: 14pt;">{{ mediaItems[listIndex].title }}</span>
-            </div>
-          </v-card-title>
-          <v-card-text class="pb-0">
-            <div>
-              <span class="caption" style="font-size: 14pt !important; font-weight: bold;">{{
-                  mediaItems[listIndex].start + "~" + mediaItems[listIndex].end
-                }}の場面</span>
-            </div>
-            <div>
-            <v-chip
-              v-for="(tag, tagIndex) in readMore(mediaItems[listIndex].tagArray)"
-              v-bind:key="mediaItems[listIndex].id + '-' + tagIndex"
-              class="my-tag-chip px-2"
-              color="lighten-5"
-              text-color="my-grey"
-              style="font-weight: normal; color: #808080; background-color: #ffffff !important; border: 1px solid #d7d7d7 !important;"
-            >
-              {{ tag }}
-            </v-chip>
-            <span v-if="mediaItems[listIndex].tagArray.length > 2">...</span>
-          </div>
-          </v-card-text>
-        </v-col>
-      </v-row>
-    </v-card> -->
   </div>
   
 </template>
