b3245caed5c22fa79049739c468b7b6fcd052007
kendai912 <kendai912@gmail.com>
2021-01-05 09:17:10 +0000

    setting OGP

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,6 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Video;
+use App\Tag;
+use App\User;
+use App\Playlist;
+
+use Carbon\Carbon;
+
+use DB;
 
 class IndexController extends Controller
 {
@@ -15,8 +23,7 @@ class IndexController extends Controller
 
     public function __construct()
     {
-        $this->facebookBot = "facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)";
-        ;
+        $this->facebookBot = 'facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)';
         $this->twitterBot = 'Twitterbot/1.0';
     }
 
@@ -26,19 +33,22 @@ class IndexController extends Controller
             $user_agent = $_SERVER['HTTP_USER_AGENT'];
             if ($user_agent == $this->twitterBot || $user_agent == $this->facebookBot) {
                 // 正規表現でurlを判定判定
-                preg_match('/(ja|en|cn|es)\/sample\/(.*?)\/(\d*)/', $_SERVER["REQUEST_URI"], $matches, PREG_OFFSET_CAPTURE);
-                if ($matches) {
+                preg_match('/^\/watch\?playlist=(?<playlistId>\d+)/', $_SERVER["REQUEST_URI"], $match);
+                if ($match) {
+                    $playlistId = $match['playlistId'];
+                    $tagId = DB::table('playlist_tag')->where('playlist_id', $playlistId)->select('tag_id')->orderBy('scene_order', 'ASC')->first()->tag_id;
+                    
+                    // get parameter contents by playlistId
                     $site_name = "YouClip";
-                    $image_url = "https://youclip-storage.s3-ap-northeast-1.amazonaws.com/thumbs/3lHThMaOW7I-3-697415540.webp";
-                    $description = "OGPのテストです";
                     $url = "https://youclip.jp";
-                    $title = "OGPテスト";
-
-                    return view('ogp')->with('site_name', $site_name)
-                                 ->with('image_url', $image_url)
-                                 ->with('description', $description)
-                                 ->with('url', $url)
-                                 ->with('title', $title);
+                    $title = Playlist::find($playlistId)->playlistName;
+                    $description = Playlist::find($playlistId)->description;
+                    if ($description == "" || $description == null) {
+                        $description = "YouTube動画のまとめ";
+                    }
+                    $image_url = "https://youclip-storage.s3-ap-northeast-1.amazonaws.com/thumbs/" . Tag::find($tagId)->preview;
+                    
+                    return view('ogp')->with('site_name', $site_name)->with('url', $url)->with('title', $title)->with('description', $description)->with('image_url', $image_url)->with('fb_app_id', env('FACEBOOK_ID'));
                 }
             }
         }
--- resources/views/ogp.blade.php
+++ resources/views/ogp.blade.php
@@ -1,27 +1,25 @@
 <!doctype html>
-<html lang="{{ app()->getLocale() }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
-  <meta name="csrf-token" content="{{ csrf_token() }}">
-  <link rel="shortcut icon" href="/images/favicon.ico"/>
-  <title>{{ config('app.name', 'Laravel') }}</title>
-  <link rel="dns-prefetch" href="//fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-  <title>{{$title}}</title>
-  <meta property="og:title" content="{{$title}}">
-  <meta property="og:image" content="{{$image_url}}">
-  <meta property="og:description" content="{{$description}}">
-  <meta property="og:url" content="{{$url}}">
-  <meta property="og:type" content="website">
-  <meta property="og:site_name" content="{{$site_name}}">
-  <meta name="twitter:site" content="{{$url}}">
-  <meta name="twitter:card" content="photo">
-  <meta name="twitter:title" content="{{$title}}">
-  <meta name="twitter:image" content="{{$image_url}}">
-  <meta name="twitter:description" content="{{$description}}">
+  <title>{{ config('app.name') }}</title>
 
+  <meta property="og:site_name" content="{{$site_name}}">
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="{{$url}}">
+  <meta property="og:title" content="{{$title}}">
+  <meta property="og:description" content="{{$description}}">
+  <meta property="og:image" content="{{$image_url}}">
   <meta property="fb:app_id" content="{{$fb_app_id}}">
+  
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:site" content="@Kendai">
+  <meta name="twitter:url" content="{{$url}}">
+  <meta name="twitter:title" content="{{$title}}">
+  <meta name="twitter:description" content="{{$description}}">
+  <meta name="twitter:image" content="{{$image_url}}">
+
 </head>
   <body>
   </body>
