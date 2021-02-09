0614357e641eabd68b09824dc031ac12369bba8c
Kendai <kendai912@gmail.com>
2021-02-10 02:48:54 +0900

    fixed structured data

--- resources/assets/js/App.vue
+++ resources/assets/js/App.vue
@@ -25,76 +25,7 @@ export default {
     Footer,
   },
   data() {
-    return {
-      jsonld: [
-        {
-          "@context": "https://schema.org",
-          "@type": "BreadcrumbList",
-          itemListElement: [
-            {
-              "@type": "ListItem",
-              position: 1,
-              name: "ホーム",
-              item: "https://youclip.jp",
-            },
-            {
-              "@type": "ListItem",
-              position: 2,
-              name: "再生",
-              item: "https://youclip.jp/watch",
-            },
-          ],
-        },
-        {
-          "@context": "https://schema.org",
-          "@type": "BreadcrumbList",
-          itemListElement: [
-            {
-              "@type": "ListItem",
-              position: 1,
-              name: "まとめ作成",
-              item: "https://youclip.jp/youtube",
-            },
-          ],
-        },
-        {
-          "@context": "https://schema.org",
-          "@type": "BreadcrumbList",
-          itemListElement: [
-            {
-              "@type": "ListItem",
-              position: 1,
-              name: "マイページ",
-              item: "https://youclip.jp/mypage",
-            },
-            {
-              "@type": "ListItem",
-              position: 2,
-              name: "ログイン",
-              item: "https://youclip.jp/login",
-            },
-          ],
-        },
-        {
-          "@context": "https://schema.org",
-          "@type": "BreadcrumbList",
-          itemListElement: [
-            {
-              "@type": "ListItem",
-              position: 1,
-              name: "マイページ",
-              item: "https://youclip.jp/mypage",
-            },
-            {
-              "@type": "ListItem",
-              position: 2,
-              name: "新規登録",
-              item: "https://youclip.jp/register",
-            },
-          ],
-        },
-      ],
-    };
+    return {};
   },
   mixins: [myMixin],
   computed: {
@@ -135,7 +66,6 @@ export default {
   },
 };
 </script>
-<script v-html="jsonld" , type="application/ld+json"></script>
 
 <style>
 @import "../css/styles.css";
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,6 +9,76 @@
   <script src="{{ asset('/js/app.js') }}" defer></script>
   <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
 
+  <script type="application/ld+json">
+  {
+    "@context": "https://schema.org",
+    "@type": "BreadcrumbList",
+    "itemListElement": [
+      {
+        "@type": "ListItem",
+        "position": 1,
+        "name": "ホーム",
+        "item": "https://youclip.jp"
+      },
+      {
+        "@type": "ListItem",
+        "position": 2,
+        "name": "再生",
+        "item": "https://youclip.jp/watch"
+      }
+    ]
+  },
+  {
+    "@context": "https://schema.org",
+    "@type": "BreadcrumbList",
+    "itemListElement": [
+      {
+        "@type": "ListItem",
+        "position": 1,
+        "name": "まとめ作成",
+        "item": "https://youclip.jp/youtube"
+      }
+    ]
+  },
+  {
+    "@context": "https://schema.org",
+    "@type": "BreadcrumbList",
+    "itemListElement": [
+      {
+        "@type": "ListItem",
+        "position": 1,
+        "name": "マイページ",
+        "item": "https://youclip.jp/mypage"
+      },
+      {
+        "@type": "ListItem",
+        "position": 2,
+        "name": "ログイン",
+        "item": "https://youclip.jp/login"
+      }
+    ]
+  },
+  {
+    "@context": "https://schema.org",
+    "@type": "BreadcrumbList",
+    "itemListElement": [
+      {
+        "@type": "ListItem",
+        "position": 1,
+        "name": "マイページ",
+        "item": "https://youclip.jp/mypage"
+      },
+      {
+        "@type": "ListItem",
+        "position": 2,
+        "name": "新規登録",
+        "item": "https://youclip.jp/register"
+      }
+    ]
+  }
+</script>
+
+
   <!-- favicon -->
   <link rel="shortcut icon" href="{{ asset('/favicon_clip.ico') }}"  type='image/x-icon'>
 
