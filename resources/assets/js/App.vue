c1b66c9dde963d0485d212f34752b8d56b868479
Kendai <kendai912@gmail.com>
2021-02-10 02:28:39 +0900

    added BreadcrumbList

--- resources/assets/js/App.vue
+++ resources/assets/js/App.vue
@@ -24,6 +24,78 @@ export default {
     Snackbar,
     Footer,
   },
+  data() {
+    return {
+      jsonld: [
+        {
+          "@context": "https://schema.org",
+          "@type": "BreadcrumbList",
+          itemListElement: [
+            {
+              "@type": "ListItem",
+              position: 1,
+              name: "ホーム",
+              item: "https://youclip.jp",
+            },
+            {
+              "@type": "ListItem",
+              position: 2,
+              name: "再生",
+              item: "https://youclip.jp/watch",
+            },
+          ],
+        },
+        {
+          "@context": "https://schema.org",
+          "@type": "BreadcrumbList",
+          itemListElement: [
+            {
+              "@type": "ListItem",
+              position: 1,
+              name: "まとめ作成",
+              item: "https://youclip.jp/youtube",
+            },
+          ],
+        },
+        {
+          "@context": "https://schema.org",
+          "@type": "BreadcrumbList",
+          itemListElement: [
+            {
+              "@type": "ListItem",
+              position: 1,
+              name: "マイページ",
+              item: "https://youclip.jp/mypage",
+            },
+            {
+              "@type": "ListItem",
+              position: 2,
+              name: "ログイン",
+              item: "https://youclip.jp/login",
+            },
+          ],
+        },
+        {
+          "@context": "https://schema.org",
+          "@type": "BreadcrumbList",
+          itemListElement: [
+            {
+              "@type": "ListItem",
+              position: 1,
+              name: "マイページ",
+              item: "https://youclip.jp/mypage",
+            },
+            {
+              "@type": "ListItem",
+              position: 2,
+              name: "新規登録",
+              item: "https://youclip.jp/register",
+            },
+          ],
+        },
+      ],
+    };
+  },
   mixins: [myMixin],
   computed: {
     ...mapGetters({
@@ -63,6 +135,8 @@ export default {
   },
 };
 </script>
+<script v-html="jsonld" , type="application/ld+json"></script>
+
 <style>
 @import "../css/styles.css";
 </style>
