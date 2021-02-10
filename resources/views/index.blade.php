31e16fd21d5eee6b4e9d25bfb2421fabfc6e108b
Kendai <kendai912@gmail.com>
2021-02-11 04:21:55 +0900

    removed cdn jquery and added async tag to script app.js

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,8 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <!-- Scripts -->
-  <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-  <script src="{{ asset('/js/app.js') }}" defer></script>
+  <script src="{{ asset('/js/app.js') }}" async></script>
 
   <script type="application/ld+json">
   {
@@ -81,13 +80,6 @@
   <!-- favicon -->
   <link rel="shortcut icon" href="{{ asset('/favicon_clip.ico') }}"  type='image/x-icon'>
 
-  <!-- Fonts -->
-  {{-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Merriweather|Roboto:400">
-  <link rel="stylesheet" href="https://unpkg.com/ionicons@4.2.2/dist/css/ionicons.min.css">
-
-  <!-- fontawsom -->
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous"> --}}
-  
 </head>
 <body>
   <div id="app"></div>
