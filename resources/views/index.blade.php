1ea4e40c8db9b97932a7cb68633b55e5ec81754c
Kendai <kendai912@gmail.com>
2021-03-31 03:30:37 +0900

    added microsoft clarity

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,6 +6,13 @@
   <link rel="shortcut icon" href="{{ asset('/favicon_clip.ico') }}"  type='image/x-icon'>
   <link rel="manifest" href="{{ asset('/manifest.json') }}">
   <link rel="preconnect" href="https://youclip-storage.s3-ap-northeast-1.amazonaws.com" />
+  <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "61qdzvn4mj");
+</script>
 </head>
 <body>
   <div id="app"></div>
