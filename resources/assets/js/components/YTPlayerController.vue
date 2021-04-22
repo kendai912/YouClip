191a5714c97ef47364db781ffe8ee58b5c2d8119
Kendai <kendai912@gmail.com>
2021-04-22 22:36:46 +0900

    changed unmute button color

--- resources/assets/js/components/YTPlayerController.vue
+++ resources/assets/js/components/YTPlayerController.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="iframeHeight">
     <div v-if="isMutedDefault" class="unmuteDefaultBtn">
-      <v-btn color="grey" height="32" v-on:click="unmuteDefault">
+      <v-btn color="red lighten-3" height="32" v-on:click="unmuteDefault">
         <v-icon left>
           volume_off
         </v-icon>
